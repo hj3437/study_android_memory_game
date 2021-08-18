1e1913a5bdaeddae9b7815a7e56c54d1cf7b38bb
HyukJin Kwon <insort@naver.com>
2021-08-19 00:53:36 +0900

    Add icons

---
+++ app/src/main/java/com/hurdle/memorygame/Constants.kt
@@ -0,0 +1,16 @@
+package com.hurdle.memorygame
+
+val CARD_ICONS = listOf(
+    R.drawable.ic_baseline_anchor_24,
+    R.drawable.ic_baseline_beach_access_24,
+    R.drawable.ic_baseline_bug_report_24,
+    R.drawable.ic_baseline_fastfood_24,
+    R.drawable.ic_baseline_fitness_center_24,
+    R.drawable.ic_baseline_free_breakfast_24,
+    R.drawable.ic_baseline_grass_24,
+    R.drawable.ic_baseline_house_24,
+    R.drawable.ic_baseline_palette_24,
+    R.drawable.ic_baseline_sports_esports_24,
+    R.drawable.ic_baseline_train_24,
+    R.drawable.ic_baseline_whatshot_24,
+)
